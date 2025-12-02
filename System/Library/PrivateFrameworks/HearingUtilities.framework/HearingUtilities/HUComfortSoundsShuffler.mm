5f1efa563096b95a881ac0ed171ac82aaef82c47
samhenrigold <49251320+samhenrigold@users.noreply.github.com>
2025-12-02 05:19:09 -0500

    rename variables

--- System/Library/PrivateFrameworks/HearingUtilities.framework/HearingUtilities/HUComfortSoundsShuffler.mm
+++ System/Library/PrivateFrameworks/HearingUtilities.framework/HearingUtilities/HUComfortSoundsShuffler.mm
@@ -1,93 +1,93 @@
 @interface HUComfortSoundsShuffler
-+ (HUComfortSoundsShuffler)shufflerWithArray:(id)a3;
-- (HUComfortSoundsShuffler)initWithArray:(id)a3;
++ (HUComfortSoundsShuffler)shufflerWithArray:(id)array;
+- (HUComfortSoundsShuffler)initWithArray:(id)array;
 - (id)nextObject;
 - (void)shuffle;
 @end
 
 @implementation HUComfortSoundsShuffler
 
-+ (HUComfortSoundsShuffler)shufflerWithArray:(id)a3
++ (HUComfortSoundsShuffler)shufflerWithArray:(id)array
 {
-  v3 = a3;
-  v4 = [[HUComfortSoundsShuffler alloc] initWithArray:v3];
+  inputArray = array;
+  shuffler = [[HUComfortSoundsShuffler alloc] initWithArray:inputArray];
 
-  return v4;
+  return shuffler;
 }
 
-- (HUComfortSoundsShuffler)initWithArray:(id)a3
+- (HUComfortSoundsShuffler)initWithArray:(id)array
 {
-  v4 = a3;
-  v9.receiver = self;
-  v9.super_class = HUComfortSoundsShuffler;
-  v5 = [(HUComfortSoundsShuffler *)&v9 init];
-  if (v5)
+  inputArray = array;
+  superCall.receiver = self;
+  superCall.super_class = HUComfortSoundsShuffler;
+  instance = [(HUComfortSoundsShuffler *)&superCall init];
+  if (instance)
   {
-    v6 = [MEMORY[0x1E695DF70] arrayWithArray:v4];
-    array = v5->_array;
-    v5->_array = v6;
+    mutableCopy = [MEMORY[0x1E695DF70] arrayWithArray:inputArray];
+    oldArray = instance->_array;
+    instance->_array = mutableCopy;
 
-    [(HUComfortSoundsShuffler *)v5 shuffle];
-    v5->_index = 0;
+    [(HUComfortSoundsShuffler *)instance shuffle];
+    instance->_index = 0;
   }
 
-  return v5;
+  return instance;
 }
 
 - (id)nextObject
 {
-  v2 = self;
-  objc_sync_enter(v2);
-  array = v2->_array;
-  v4 = v2->_index + 1;
-  v2->_index = v4;
-  if (v4 >= [(NSMutableArray *)array count])
+  this = self;
+  objc_sync_enter(this);
+  array = this->_array;
+  nextIndex = this->_index + 1;
+  this->_index = nextIndex;
+  if (nextIndex >= [(NSMutableArray *)array count])
   {
-    [(HUComfortSoundsShuffler *)v2 shuffle];
-    v2->_index = 0;
+    [(HUComfortSoundsShuffler *)this shuffle];
+    this->_index = 0;
   }
 
-  if ([(NSMutableArray *)v2->_array count]<= v2->_index)
+  if ([(NSMutableArray *)this->_array count]<= this->_index)
   {
-    v5 = 0;
+    result = 0;
   }
 
   else
   {
-    v5 = [(NSMutableArray *)v2->_array objectAtIndex:?];
+    result = [(NSMutableArray *)this->_array objectAtIndex:this->_index];
   }
 
-  objc_sync_exit(v2);
+  objc_sync_exit(this);
 
-  return v5;
+  return result;
 }
 
 - (void)shuffle
 {
   if ([(NSMutableArray *)self->_array count]>= 2)
   {
-    v7 = [(NSMutableArray *)self->_array lastObject];
+    lastObject = [(NSMutableArray *)self->_array lastObject];
     if ([(NSMutableArray *)self->_array count]!= 1)
     {
-      v3 = 0;
-      v4 = 0;
+      remainingDecrement = 0;
+      currentIndex = 0;
       do
       {
-        [(NSMutableArray *)self->_array exchangeObjectAtIndex:v4 withObjectAtIndex:v4 + arc4random_uniform(v3 + [(NSMutableArray *)self->_array count])];
-        ++v4;
-        --v3;
+        [(NSMutableArray *)self->_array exchangeObjectAtIndex:currentIndex withObjectAtIndex:currentIndex + arc4random_uniform(remainingDecrement + [(NSMutableArray *)self->_array count])];
+        ++currentIndex;
+        --remainingDecrement;
       }
 
-      while (v4 < [(NSMutableArray *)self->_array count]- 1);
+      while (currentIndex < [(NSMutableArray *)self->_array count]- 1);
     }
 
-    v5 = [(NSMutableArray *)self->_array firstObject];
+    firstObject = [(NSMutableArray *)self->_array firstObject];
 
-    v6 = v7;
-    if (v5 == v7)
+    lastObjectCopy = lastObject;
+    if (firstObject == lastObject)
     {
       [(NSMutableArray *)self->_array exchangeObjectAtIndex:0 withObjectAtIndex:[(NSMutableArray *)self->_array count]- 1];
-      v6 = v7;
+      lastObjectCopy = lastObject;
     }
   }
 }
